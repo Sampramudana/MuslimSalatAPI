5b7bf7ed8bc0f6c5e75cbeb7c1264162a0270f39
Sampramudana <32828518+Sampramudana@users.noreply.github.com>
2018-08-27 21:19:45 +0700

    Design Layout Finished

--- app/src/main/java/com/pramudana/sam/muslimsalatapi/Activity/MainActivity.java
+++ app/src/main/java/com/pramudana/sam/muslimsalatapi/Activity/MainActivity.java
@@ -2,10 +2,13 @@ package com.pramudana.sam.muslimsalatapi.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.pramudana.sam.muslimsalatapi.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView shubuh, shurooq, dzuhur, ashar, maghrib, isya, state, city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
